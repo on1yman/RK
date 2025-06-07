54471db377692154dfcfbd8443039074799db9eb
on1yman <nik.sharipowtop@gmail.com>
2025-06-08 02:12:05 +0300

    Добавил тесты

---
+++ _build/CMakeFiles/AbstractFactory.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "AbstractFactory"
+  "AbstractFactory.pdb"
+  "CMakeFiles/AbstractFactory.dir/src/AbstractFactory/Conceptual/main.cc.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AbstractFactory.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
