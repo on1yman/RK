105ae9eeafa71533a6cabed1a92aaa47f6a12208
on1yman <nik.sharipowtop@gmail.com>
2025-06-08 03:04:26 +0300

    Remove build directory from repo

--- _build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/onlyman/on1yman/RK
-# Build directory: /home/onlyman/on1yman/RK/_build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(AbstractFactoryTests "/home/onlyman/on1yman/RK/_build/run_tests")
-set_tests_properties(AbstractFactoryTests PROPERTIES  _BACKTRACE_TRIPLES "/home/onlyman/on1yman/RK/CMakeLists.txt;19;add_test;/home/onlyman/on1yman/RK/CMakeLists.txt;0;")
